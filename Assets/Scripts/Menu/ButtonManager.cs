6426fa8702f06d9e9c734604bae518391e504e4a
samjerry <jerry.sam@ziggo.nl>
2017-11-13 10:55:52 +0100

    jerry's commit1

---
+++ Assets/Scripts/Menu/ButtonManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public Button Play;
+	public Button Settings;
+	public Button Back;
+	public Button Exit;
+
+	private GameObject go_main;
+	private GameObject go_settings;
+
+
+	void Awake(){
+
+		go_main = GameObject.Find ("Main");
+		go_settings = GameObject.Find ("Settings");
+
+		go_settings.SetActive (false); 
+
+	}
+
+	void Start()
+	{
+		Button btn_play = Play.GetComponent<Button>();
+		Button btn_settings = Settings.GetComponent<Button>(); 
+		Button btn_back = Back.GetComponent<Button>(); 
+		Button btn_exit = Exit.GetComponent<Button>();
+
+		btn_play.onClick.AddListener(delegate() { TaskOnClick("Play"); });
+		btn_settings.onClick.AddListener(delegate() { TaskOnClick("Settings"); });
+		btn_back.onClick.AddListener(delegate() { TaskOnClick("Back"); });
+		btn_exit.onClick.AddListener(delegate() { TaskOnClick("Exit"); });
+
+	}
+
+	void TaskOnClick(string option){
+		if (option == "Play") {
+			SceneManager.LoadScene ("JerryTestScene");
+			Debug.Log ("Play");
+		} 
+		else if (option == "Settings") {
+			go_main.SetActive (false);
+			go_settings.SetActive (true);
+		}
+		else if (option == "Back") {
+			go_main.SetActive (true);
+			go_settings.SetActive (false);
+		}
+		else {
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerHealthManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public float StartingHealth = 100;                            
+    public float CurrentHealth; 
+	
+	public Image HealthBar;                                
+
+	private float _TestDmg = 1;
+	private float _dmg = 25;
+	private float _DelayHealthDecay = 0.01f;
+
+    private bool _isDead;
+    private bool _Damaged;
+
+
+    void Awake (){
+        CurrentHealth = StartingHealth;
+    }
+
+    void Update (){
+
+		HealthBar.fillAmount = CurrentHealth/StartingHealth;
+
+		if (Input.GetKeyDown (KeyCode.R) && !_isDead) {
+			CurrentHealth -= _TestDmg;
+			if (CurrentHealth <= 0){
+				Death ();
+			}
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.T) && !_Damaged && !_isDead) {
+			_Damaged = true;
+			StartCoroutine (HealthDecay());
+//			CurrentHealth -= _TestDmg;
+//			if (CurrentHealth <= 0){
+//				Death ();
+//			}
+		} 
+    }
+
+    public void TakeDamage (int amount){
+        _Damaged = true;
+        CurrentHealth -= amount;
+
+		if(CurrentHealth <= 0 && !_isDead)
+        {
+            Death ();
+        }
+    }
+
+	public IEnumerator HealthDecay () {
+		for (int i = 0; i < _dmg; i++) {
+				CurrentHealth -= _TestDmg;
+				if (CurrentHealth <= 0) {
+					Death ();
+				}
+			yield return new WaitForSeconds (_DelayHealthDecay);
+			}
+		StopCoroutine ("HealthDecay");
+		_Damaged = false;
+		}
+
+ 	public void Death (){ 
+        _isDead = true;
+		CurrentHealth = 0;
+		Time.timeScale = 0;
+	 // <WeaponScriptName>.enabled = false;
+	}
+}
